ba38bd56dba44202ac8af864d712a10bc2ab3025
BuildTools <steve4744@gmail.com>
2019-01-31 15:35:45 +0000

    add new version checker

---
+++ src/io/github/steve4744/whatisthis/VersionChecker.java
@@ -0,0 +1,52 @@
+/*
+ * MIT License
+
+Copyright (c) 2018 steve4744
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+ */
+package io.github.steve4744.whatisthis;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.bukkit.Bukkit;
+
+public class VersionChecker {
+	
+	public static String getVersion() {
+		try {
+			HttpURLConnection con = (HttpURLConnection) new URL("https://www.spigotmc.org/api/general.php").openConnection();
+			con.setDoOutput(true);
+			con.setRequestMethod("POST");
+			con.getOutputStream().write(("key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=xxxxx").getBytes("UTF-8"));
+			String version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
+			if (version.length() <= 7) {
+				return version;
+			}
+		} catch (Exception ex) {
+			Bukkit.getLogger().info("Failed to check for an update on Spigot.");
+		}
+		return "error";
+	}
+
+}
--- src/io/github/steve4744/whatisthis/WhatIsThis.java
+++ src/io/github/steve4744/whatisthis/WhatIsThis.java
@@ -3,28 +3,29 @@ package io.github.steve4744.whatisthis;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import io.github.steve4744.whatisthis.metrics.Metrics;
 
-
 public class WhatIsThis extends JavaPlugin {
-	
+
 	private static WhatIsThis instance;
 	private String version;
 	private ScoreboardManager scoreboardManager;
 
 	@Override
 	public void onEnable() {
-		
+
 		instance = this;
 		this.saveDefaultConfig();
 		getConfig().options().copyDefaults(true);
 		saveConfig();
-		
+
 		version = this.getDescription().getVersion();
 		setupPlugin();
-		
+
 		new Metrics(this);
+		checkForUpdate();
 	}
 
 	@Override
@@ -49,6 +50,25 @@ public class WhatIsThis extends JavaPlugin {
 			PluginManager pm = Bukkit.getPluginManager();
 			pm.registerEvents(new WhatIsThisListener(), this);
 		}
+	}
+
+	private void checkForUpdate() {
+		if (!getConfig().getBoolean("check_for_update", true)) {
+			return;
+		}
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				String latestVersion = VersionChecker.getVersion();
+				if (latestVersion == "error") {
+					getLogger().info("Error attempting to check for new version. Please report it here: https://www.spigotmc.org/threads/whatisthis.xxxxx/");
+				} else {
+					if (!version.equals(latestVersion)) {
+						getLogger().info("New version " + latestVersion + " available on Spigot: https://www.spigotmc.org/resources/whatisthis.zzzzz/");
+					}
+				}
+			}
+		}.runTaskLaterAsynchronously(this, 40L);
 	}
 
 }
