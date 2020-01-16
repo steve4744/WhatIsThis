153f08d632e7c4f2fff1ce1cfbf8f76228abd37a
BuildTools <steve4744@gmail.com>
2020-01-16 21:20:54 +0000

    fix version checker

--- src/io/github/steve4744/whatisthis/VersionChecker.java
+++ src/io/github/steve4744/whatisthis/VersionChecker.java
@@ -35,11 +35,11 @@ public class VersionChecker {
 	
 	public static String getVersion() {
 		try {
-			HttpURLConnection con = (HttpURLConnection) new URL("https://www.spigotmc.org/api/general.php").openConnection();
+			HttpURLConnection con = (HttpURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=65050").openConnection();
 			con.setDoOutput(true);
-			con.setRequestMethod("POST");
-			con.getOutputStream().write(("key=98BE0FE67F88AB82B4C197FAF1DC3B69206EFDCC4D3B80FC83A00037510B99B4&resource=65050").getBytes("UTF-8"));
+			con.setRequestMethod("GET");
 			String version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
+			con.disconnect();
 			if (version.length() <= 7) {
 				return version;
 			}
